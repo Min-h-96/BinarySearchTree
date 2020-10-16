92805213776f70ed05664b76eab5bf6c1df45a56
Min-h-96 <asdf6256@gmail.com>
2020-10-17 00:15:34 +0900

    Print node(temporary)

--- BST.py
+++ BST.py
@@ -27,6 +27,18 @@ class BinarySearchTree(object):
             else:
                 currentNode.left = Node(data)
 
+    def print(self):
+        print('루트 노드:', self.root.data)
+        self.printChild(self.root)
+
+    def printChild(self, currentNode):
+        if currentNode.right:
+            print(currentNode.data, '의 오른쪽 자식 노드 :', currentNode.right.data)
+            self.printChild(currentNode.right)
+        if currentNode.left:
+            print(currentNode.data, '의 왼쪽 자식 노드 :', currentNode.left.data)
+            self.printChild(currentNode.left)
+
 
 if __name__ == "__main__":
     arr = [40, 4, 34, 45, 14, 55, 48, 13, 15, 49, 47]
@@ -34,3 +46,4 @@ if __name__ == "__main__":
     bst = BinarySearchTree()
     for i in arr:
         bst.insert(i)
+    bst.print()
